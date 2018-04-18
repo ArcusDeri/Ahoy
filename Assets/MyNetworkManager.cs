e1a64c2290bcc2bde60783d678a6dc2edade707a
Marcin Michniak <michniak@yandex.com>
2018-04-18 16:51:57 +0200

    Resize start host button. Log ip address when connected with a client. IP address of the host is needed to be typed in. Print  "." when trying to connect.

--- Assets/MyNetworkManager.cs
+++ Assets/MyNetworkManager.cs
@@ -15,11 +15,25 @@ public class MyNetworkManager : NetworkManager {
 	}
 
 	public void MyStartHost(){
-		Debug.Log("Starting host at " + Time.timeSinceLevelLoad);
+		Debug.Log(Time.timeSinceLevelLoad + " host start requested.");
 		StartHost();
 	}
 
 	public override void OnStartHost(){
-		Debug.Log("Host started at " + Time.timeSinceLevelLoad);
+		Debug.Log(Time.timeSinceLevelLoad + " host started.");
+	}
+
+	public override void OnStartClient(NetworkClient myClient){
+		Debug.Log(Time.timeSinceLevelLoad + " client start requested.");
+		InvokeRepeating("PrintDots",0,1);
+	}
+
+	public override void OnClientConnect(NetworkConnection conn){
+		Debug.Log(Time.timeSinceLevelLoad + " client connected to IP: " + conn.address);
+		CancelInvoke("PrintDots");
+	}
+
+	private void PrintDots(){
+		Debug.Log(".");
 	}
 }
