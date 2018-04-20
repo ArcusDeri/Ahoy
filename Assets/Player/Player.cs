7b39d7fb19c60c91d3100670a309cf1e3bf20294
Marcin Michniak <michniak@yandex.com>
2018-04-20 12:38:45 +0200

    Import CrossPlatformInput. Add virtual joy and move the player with it. Players move independently of each other. However the host doesn't receive the change of client's position.

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+	private Vector3 InputValue;
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isLocalPlayer)
+			return;
+		InputValue.x = CrossPlatformInputManager.GetAxis("Horizontal") * Time.deltaTime;
+		InputValue.y = 0;
+		InputValue.z = CrossPlatformInputManager.GetAxis("Vertical") * Time.deltaTime;
+		gameObject.transform.Translate(InputValue);
+	}
+}
